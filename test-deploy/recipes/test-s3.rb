838a3839d45a966de929d1ef83388a35d8a7cbfb
tp6m4fu6250071 <tp6m4fu6250071@gmail.com>
2019-10-18 18:50:47 +0800

    test

--- test-deploy/recipes/test-s3.rb
+++ test-deploy/recipes/test-s3.rb
@@ -1,8 +1,4 @@
 #require 'aws-sdk'
 
-node[:deploy].each do |deploy|
-  execute "test-s3" do
-    Chef::Log.info("********** HelloWorld123 **********")
-  end
-end
+Chef::Log.info("********** HelloWorld123 **********")
 
