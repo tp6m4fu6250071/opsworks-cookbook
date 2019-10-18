01e6c91325d2e3f4c78c59048f86807e42b989da
tp6m4fu6250071 <tp6m4fu6250071@gmail.com>
2019-10-18 18:33:02 +0800

    tes

--- test-deploy/recipes/test-s3.rb
+++ test-deploy/recipes/test-s3.rb
@@ -1,23 +1,8 @@
-require 'aws-sdk'
+#require 'aws-sdk'
 
-Chef::Log.info("********** HelloWorld123 **********")
-
-s3 = AWS::S3.new
-
-# Set bucket and object name
-obj = s3.buckets['test-opsworks-s3-role-appbucket-h74iqpaoyoru'].objects['object.txt']
-
-# Read content to variable
-file_content = obj.read
-
-# Log output (optional)
-Chef::Log.info(file_content)
-
-# Write content to file
-file '/tmp/myobject.txt' do
-  owner 'root'
-  group 'root'
-  mode '0755'
-  content file_content
-  action :create
+node[:deploy].each do |deploy|
+  execute "test-s3" do
+    Chef::Log.info("********** HelloWorld123 **********")
+  end
 end
+
